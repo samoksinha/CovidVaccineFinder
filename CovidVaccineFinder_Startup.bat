19389ebf07843544417ca9832c3f496f28fc6c3f
Samok Sinha <samok.sinha@gmail.com>
2021-05-18 17:34:54 +0530

    Add files via upload

--- CovidVaccineFinder_Startup.bat
+++ CovidVaccineFinder_Startup.bat
@@ -5,6 +5,7 @@ rem Date for which vaccine is searched i.e. (17-05-2021)
 rem Type of Vaccine i.e. (COVAXIN or COVISHIELD currently)
 rem Age limit i.e. (18 or 45 currently)
 rem Vaccine Dose i.e. (1 for First dose or 2 for Second Dose)
+rem Single Day/7 Days Slot i.e. (1 for Single Day or 7 for Seven Days)
 rem Time interval for which API will be called i.e. (30000 in miliseconds which is 30 seconds)
 
-java -jar CovidVaccineFinder.jar 725 17-05-2021 COVAXIN 45 2 150000
+java -jar CovidVaccineFinder.jar 725 17-05-2021 COVAXIN 45 2 1 150000
